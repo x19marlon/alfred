b9a06c9fa65958c6c827d9ae510d7b3d8ca5c2d1
DanielRobayo12 <daniel6968.felipe@gmail.com>
2026-08-31 00:11:18 -0500

    change urdf to xacro

--- alfredr1/launch/unitree_r1.launch.py
+++ alfredr1/launch/unitree_r1.launch.py
@@ -1,34 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, PathJoinSubstitution
-from launch_ros.parameter_descriptions import ParameterValue
-from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 import os
-
 
 def generate_launch_description():
     
     pkg_share = get_package_share_directory('alfredr1')
-    rviz_file = os.path.join(pkg_share,'rviz2','rviz2.rviz')
-
-    xacro_file = PathJoinSubstitution([
-        FindPackageShare('alfredr1'),
-        'urdf',
-        'r1_urdf.urdf.xacro'
-    ])
     
+    urdf_file = os.path.join(pkg_share,'urdf','R1.urdf')
+    rviz_file = os.path.join(pkg_share,'rviz2','rviz2.rviz')
+    
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
         
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{'robot_description': ParameterValue(
-                Command([
-                    'xacro ', xacro_file
-                ]),
-                value_type=str
-            )}]
+            parameters=[{'robot_description': robot_desc}]
         ),        
         Node(
             package='rviz2',
--- alfredr1/setup.py
+++ alfredr1/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share',package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share',package_name, 'urdf'), glob('urdf/*.xacro')),
         (os.path.join('share',package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share',package_name, 'urdf/meshes'), glob('urdf/meshes/*')),
         (os.path.join('share', package_name, 'rviz2'), glob('rviz2/*.rviz')),
